5a0c5658cda97a9a44376d1109715a87c985b455
Juan Ignacio Villegas <juaniriver01@gmail.com>
2026-02-04 17:29:45 -0300

    Proyecto final simulador ecommerce

--- js/main.js
+++ js/main.js
@@ -2,46 +2,75 @@ let carrito = [];
 let total = 0;
 
 // DOM
-const formulario = document.getElementById("formProducto");
-const lista = document.getElementById("listaProductos");
+const listaProductos = document.getElementById("listaProductos");
+const listaCarrito = document.getElementById("listaCarrito");
 const totalSpan = document.getElementById("total");
+
+
+// Cargar carrito desde localStorage
+carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+total = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+totalSpan.textContent = total;
+
+carrito.forEach(prod => {
+  const li = document.createElement("li");
+  li.textContent = `${prod.nombre} - $${prod.precio}`;
+  listaProductos.appendChild(li);
+
+ 
+});
+
+// Guardar carrito
+function guardarCarrito() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
 
 // Cargar productos desde JSON
 fetch("data/productos.json")
   .then(response => response.json())
-  productos.forEach(producto => {
-  const li = document.createElement("li");
-  li.textContent = `${producto.nombre} - $${producto.precio}`;
-  lista.appendChild(li);
-});
+  .then(productos => {
+    productos.forEach(producto => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+        ${producto.nombre} - $${producto.precio}
+        <button onclick="agregarProducto('${producto.nombre}', ${producto.precio})">
+          Agregar
+        </button>
+      `;
+      listaCarrito.appendChild(li);
 
-// Evento formulario
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
+    });
+  });
 
-  const nombre = document.getElementById("nombre").value;
-  const precio = Number(document.getElementById("precio").value);
-
-  agregarProducto(nombre, precio);
-  formulario.reset();
-});
-
-// Función principal
+// Función agregar producto al carrito
 function agregarProducto(nombre, precio) {
   carrito.push({ nombre, precio });
   total += precio;
 
   const li = document.createElement("li");
   li.textContent = `${nombre} - $${precio}`;
-  lista.appendChild(li);
+  listaCarrito.appendChild(li);
+
+
 
   totalSpan.textContent = total;
+  guardarCarrito();
+
+  Swal.fire({
+    icon: "success",
+    title: "Producto agregado",
+    text: "Se agregó correctamente al carrito",
+    timer: 1200,
+    showConfirmButton: false
+  });
 }
-Swal.fire({
-  icon: "success",
-  title: "Producto agregado",
-  text: "Se agregó correctamente al carrito",
-  timer: 1200,
-  showConfirmButton: false
+document.getElementById("vaciar").addEventListener("click", () => {
+  carrito = [];
+  total = 0;
+  listaCarrito.innerHTML = "";
+  totalSpan.textContent = 0;
+  localStorage.removeItem("carrito");
+
+  Swal.fire("Carrito vacío", "", "info");
 });
 
